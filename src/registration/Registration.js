8054d298dfb6711be98fb99e4d245e714269a6a0
Ryan Vaughan <vaugharj@uwec.edu>
2020-05-17 20:50:35 -0500

    bit of client-side sanitization

--- src/registration/Registration.js
+++ src/registration/Registration.js
@@ -10,10 +10,25 @@ function Registration(props){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    const registerError = useSelector(state => state.registerError)
+    const registerError = useSelector(state => state.registerError);
+    const [invalidPass, setInvalidPass] = useState(false);
+    const [invalidUser, setInvalidUser] = useState(false);
 ;    
     function onRegister(username, password, email){
-        dispatch(startRegistering(username, password, email))
+        if(username.length < 3){
+            setInvalidUser(true);
+            setInvalidPass(false);
+        }
+        else if(password.length < 8){
+            setInvalidPass(true);
+            setInvalidUser(false);
+        }
+        else{
+            setInvalidPass(false);
+            setInvalidUser(false);
+            dispatch(startRegistering(username, password, email));
+        }
+        
     }
 
 
@@ -21,14 +36,20 @@ function Registration(props){
     return(
         <div className="registration">
             <div className="registration-header">Register</div>
-            <div className="registration-label username">Username</div>
+            <div className="registration-label username">&nbsp;&nbsp;Username *</div>
             <input type="text" className="registration-field field-username" maxLength={14} onChange={(e) => setUsername(e.target.value)}></input>
-            <div className="registration-label password">Password</div>
+            <div className="registration-label password">&nbsp;&nbsp;Password *</div>
             <input type="password" className="registration-field field-password" onChange={(e) => setPassword(e.target.value)}></input>
-            <div className="registration-label email">Email</div>
+            <div className="registration-label email">&nbsp;&nbsp;Email&nbsp;&nbsp;</div>
             <input type="text" className="registration-field field-email" onChange={(e) => setEmail(e.target.value)}></input>
             <button className="registration-submit" onClick={() => onRegister(username, password, email)}>Register</button>
-            {registerError &&
+            {invalidUser &&
+            <div className="register-error">Username must contain at least 3 characters</div>
+            }
+            {invalidPass &&
+            <div className="register-error">Password must contain at least 8 characters.</div>
+            }
+            {registerError && !invalidPass && !!invalidUser &&
             <div className="register-error">Error registering.</div>
             }
         </div>
